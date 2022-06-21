444bf261f8049337ee3bce00da5f23b751aadcab
Marwin Ko <marwin.r.ko@gmail.com>
2022-06-21 13:40:15 -0700

    Update decorator_input_output_logger.py

--- decorator_input_output_logger.py
+++ decorator_input_output_logger.py
@@ -1,16 +1,23 @@
 #Credit to: https://stackoverflow.com/questions/25936746/create-a-function-decorator-that-logs-arguments
+import logging
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-def logger(prefix):
+
+def logger_(prefix):
     def decorate(f):
         def wrapper(*args, **kwargs):
-            print(prefix, f.__name__, "args", args, "kwargs", kwargs)
-            cr = f(*args, **kwargs)
-            print(prefix, f.__name__, "result", cr)
-            return cr
+            logger.info(f'{prefix} - {f.__name__} - START')
+            logger.info(f'{prefix} - {f.__name__} - INPUT - Positional Args: {args}')
+            logger.info(f'{prefix} - {f.__name__} - INPUT - Keyworod Args: {kwargs}')
+            output = f(*args, **kwargs)
+            logger.info(f'{prefix} - {f.__name__} - OUTPUT: {output}')
+            logger.info(f'{prefix} - {f.__name__} - END')
+            return output
         return wrapper
     return decorate
 
 
-@logger("test")
+@logger("STEP #1")
 def zut(a, b, foo='FOO'):
     return a+b
