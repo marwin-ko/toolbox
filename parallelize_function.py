026b71c14fe5b60597cfbdbdd6dcdbb3beb195da
Marwin Ko <marwin.r.ko@gmail.com>
2022-06-21 10:53:57 -0700

    Update and rename multiprocess_and_decorator_example.py to parallelize_function.py

--- parallelize_function.py
+++ parallelize_function.py
@@ -1,20 +1,10 @@
 import multiprocessing as mp
 
 
-def my_timer(func):
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        f = func(*args, **kwargs)
-        end = round(time.time(),2)
-        logger.info("Data Transfer Duration: %s sec", end - start)
-        return f
-    return wrapper
-
 def download(job):
   bucket, filekey = job
   # some download code
   
-@my_timer
 def parallel_download(jobs):
   pool = mp.Pool(mp.cpu_count())
   pool.map(download, jobs)
@@ -29,6 +19,6 @@ def get_jobs(df):
     jobs.append((bucket, filekey))
    return jobs
 
-
-jobs = get_jobs(df)
-parallel_download
+if __name__ == '__main__':
+    jobs = get_jobs(df)
+    parallel_download(jobs)
