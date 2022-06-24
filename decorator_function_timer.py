3785e3d9eb2fd11a420d9c0d14400bcacebc9c49
Marwin Ko <marwin.r.ko@gmail.com>
2022-06-24 10:50:36 -0700

    Update decorator_function_timer.py

--- decorator_function_timer.py
+++ decorator_function_timer.py
@@ -1,9 +1,22 @@
+import logging
 import time
-def timer_(func):
+
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def function_timer(f):
     def wrapper(*args, **kwargs):
+        title = f'RUNNING FUNCTION: {f.__name__}'
+        title_n_chars = len(title)
+        logger.info(title_n_chars * '=')
+        logger.info(title)
+        logger.info(title_n_chars * '=')
         start = time.time()
-        f = func(*args, **kwargs)
+        output = f(*args, **kwargs)
         end = round(time.time(),2)
-        logger.info("Data Transfer Duration: %s sec", end - start)
-        return f
+        logger.info("Total Function Run Duration: %s sec", end - start)
+        logger.info(title_n_chars * '=')
+        return output
     return wrapper
